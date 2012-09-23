79f9212b531f887d8f106e112af60f5fc1083381
Peter C <pmcollins2@wisc.edu>
2012-09-22 23:57:09 -0500

    Added facebook authentication and icons

--- Hillbomber/gen/com/hillbomber/R.java
+++ Hillbomber/gen/com/hillbomber/R.java
@@ -16,8 +16,10 @@ public final class R {
         public static final int padding_small=0x7f040000;
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int close=0x7f020000;
+        public static final int facebook_icon=0x7f020001;
+        public static final int ic_action_search=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
         public static final int mapview=0x7f080000;
--- Hillbomber/src/com/hillbomber/MainActivity.java
+++ Hillbomber/src/com/hillbomber/MainActivity.java
@@ -14,6 +14,8 @@ import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
@@ -21,12 +23,18 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 
+import com.facebook.android.DialogError;
+import com.facebook.android.Facebook;
+import com.facebook.android.Facebook.DialogListener;
+import com.facebook.android.FacebookError;
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 
 public class MainActivity extends MapActivity {
+
+	private SharedPreferences userPreferences;
 
 	private LocationManager locationManager;
 	private LocationListener locationListener;
@@ -36,26 +44,63 @@ public class MainActivity extends MapActivity {
 	private MapController mapController;
 	private GoogleParser googleParser;
 
+	private Facebook facebook = new Facebook("468347859863144");
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		mapView = (MapView) findViewById(R.id.mapview);
+		userPreferences = getPreferences(MODE_PRIVATE);
+		String access_token = userPreferences.getString("access_token", null);
+		long expires = userPreferences.getLong("access_expires", 0);
+		if (access_token != null && expires != 0) {
+			facebook.setAccessToken(access_token);
+			facebook.setAccessExpires(expires);
+		}
+
+		if (!facebook.isSessionValid()) {
+			facebook.authorize(this, new DialogListener() {
+				public void onComplete(Bundle values) {
+					SharedPreferences.Editor editor = userPreferences.edit();
+                    editor.putString("access_token", facebook.getAccessToken());
+                    editor.putLong("access_expires", facebook.getAccessExpires());
+                    editor.commit();
+				}
+
+				public void onFacebookError(FacebookError error) {
+				}
+
+				public void onError(DialogError e) {
+				}
+
+				public void onCancel() {
+				}
+			});
+		}
+
 		googleParser = new GoogleParser();
 
 		BufferedReader in = null;
 		String line = null;
 		try {
-			in = new BufferedReader(new InputStreamReader(
-					getConnection("http://hillbomber.herokuapp.com/trails.json")));
+			in = new BufferedReader(
+					new InputStreamReader(
+							getConnection("http://hillbomber.herokuapp.com/trails.json")));
 			while ((line = in.readLine()) != null) {
 				JSONArray routes = (JSONArray) new JSONTokener(line)
 						.nextValue();
 				for (int i = 0; i < routes.length(); i++) {
 					JSONObject route = routes.getJSONObject(i);
-                    String url = googleParser.directions(new GeoPoint((int)(route.getDouble("s_lat")*1E6),(int)(route.getDouble("s_long")*1E6)), new GeoPoint((int)(route.getDouble("e_lat")*1E6),(int)(route.getDouble("e_long")*1E6)));
-                    RouteOverlay routeOverlay = new RouteOverlay(googleParser.parse(getConnection(url)), Color.BLUE);
-                    mapView.getOverlays().add(routeOverlay);
+					String url = googleParser.directions(new GeoPoint(
+							(int) (route.getDouble("s_lat") * 1E6),
+							(int) (route.getDouble("s_long") * 1E6)),
+							new GeoPoint(
+									(int) (route.getDouble("e_lat") * 1E6),
+									(int) (route.getDouble("e_long") * 1E6)));
+					RouteOverlay routeOverlay = new RouteOverlay(
+							googleParser.parse(getConnection(url)), Color.BLUE);
+					mapView.getOverlays().add(routeOverlay);
 				}
 				mapView.invalidate();
 			}
@@ -90,6 +135,19 @@ public class MainActivity extends MapActivity {
 				.getLatitude() * 1000000), (int) (locationManager
 				.getLastKnownLocation(LocationManager.GPS_PROVIDER)
 				.getLongitude() * 1000000)));
+	}
+	
+	@Override
+	public void onResume() {    
+        super.onResume();
+        facebook.extendAccessTokenIfNeeded(this, null);
+    }
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		facebook.authorizeCallback(requestCode, resultCode, data);
 	}
 
 	@Override
