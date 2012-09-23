2e1e27f6e6e79b7860c86218b3505f070fb44263
Peter C <pmcollins2@wisc.edu>
2012-09-23 03:13:48 -0500

    Added send facebook auth and post new route

--- Hillbomber/src/com/hillbomber/MainActivity.java
+++ Hillbomber/src/com/hillbomber/MainActivity.java
@@ -1,13 +1,23 @@
 package com.hillbomber;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -22,6 +32,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager.LayoutParams;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -31,6 +42,8 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.facebook.android.AsyncFacebookRunner;
+import com.facebook.android.AsyncFacebookRunner.RequestListener;
 import com.facebook.android.DialogError;
 import com.facebook.android.Facebook;
 import com.facebook.android.Facebook.DialogListener;
@@ -46,11 +59,13 @@ public class MainActivity extends MapActivity {
 
 	private LocationManager locationManager;
 	private LocationListener locationListener;
-	
+
+	private String facebookId, facebookName;
 	private String currentRoute, currentDifficulty;
+	private Location startLocation, endLocation;
 
 	private MapView mapView;
-	private View routeView; 
+	private View routeView;
 	private Button refreshButton, newButton, startButton, endButton;
 	private EditText titleText;
 	private RadioGroup difficultyRadios;
@@ -59,36 +74,40 @@ public class MainActivity extends MapActivity {
 	private GoogleParser googleParser;
 
 	private Facebook facebook = new Facebook("468347859863144");
+	private AsyncFacebookRunner asyncFacebookRunner = new AsyncFacebookRunner(
+			facebook);
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		refreshButton = (Button)findViewById(R.id.refresh);
-		newButton = (Button)findViewById(R.id.newRoute);
-		endButton = (Button)findViewById(R.id.endRoute);
-		
-		LayoutInflater inflater = (LayoutInflater)getApplicationContext().getSystemService
-			      (Context.LAYOUT_INFLATER_SERVICE);
+		refreshButton = (Button) findViewById(R.id.refresh);
+		newButton = (Button) findViewById(R.id.newRoute);
+		endButton = (Button) findViewById(R.id.endRoute);
+
+		LayoutInflater inflater = (LayoutInflater) getApplicationContext()
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		routeView = inflater.inflate(R.layout.route, null);
 		routeView.setVisibility(View.GONE);
-		startButton = (Button)routeView.findViewById(R.id.start);
+		startButton = (Button) routeView.findViewById(R.id.start);
 		startButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				onStartRouteClicked(v);
 			}
 		});
-		titleText = (EditText)routeView.findViewById(R.id.title);
-		difficultyRadios = (RadioGroup)routeView.findViewById(R.id.difficulty);
+		titleText = (EditText) routeView.findViewById(R.id.title);
+		difficultyRadios = (RadioGroup) routeView.findViewById(R.id.difficulty);
 		addContentView(routeView, new LayoutParams());
-		
+
 		mapView = (MapView) findViewById(R.id.mapview);
 		userPreferences = getPreferences(MODE_PRIVATE);
-		String access_token = userPreferences.getString("access_token", null);
-		long expires = userPreferences.getLong("access_expires", 0);
-		if (access_token != null && expires != 0) {
-			facebook.setAccessToken(access_token);
+		String accessToken = null;//userPreferences.getString("access_token", null);
+		long expires = 0;//userPreferences.getLong("access_expires", 0);
+		facebookId = userPreferences.getString("facebook_id", null);
+		facebookName = userPreferences.getString("facebook_name", null);
+		if (accessToken != null && expires != 0) {
+			facebook.setAccessToken(accessToken);
 			facebook.setAccessExpires(expires);
 		}
 
@@ -96,17 +115,68 @@ public class MainActivity extends MapActivity {
 			facebook.authorize(this, new DialogListener() {
 				public void onComplete(Bundle values) {
 					SharedPreferences.Editor editor = userPreferences.edit();
-                    editor.putString("access_token", facebook.getAccessToken());
-                    editor.putLong("access_expires", facebook.getAccessExpires());
-                    editor.commit();
+					editor.putString("access_token", facebook.getAccessToken());
+					editor.putLong("access_expires",
+							facebook.getAccessExpires());
+					editor.commit();
+
+					asyncFacebookRunner.request("me", new RequestListener() {
+
+						@Override
+						public void onMalformedURLException(
+								MalformedURLException e, Object state) {
+							// Log.e("Facebook Error", e.toString());
+						}
+
+						@Override
+						public void onIOException(IOException e, Object state) {
+							// Log.e("Facebook Error", e.toString());
+						}
+
+						@Override
+						public void onFileNotFoundException(
+								FileNotFoundException e, Object state) {
+							// Log.e("Facebook Error", e.toString());
+						}
+
+						@Override
+						public void onFacebookError(FacebookError e,
+								Object state) {
+							// Log.e("Facebook Error", e.toString());
+						}
+
+						@Override
+						public void onComplete(String response, Object state) {
+							Log.i("Facebook Me", response);
+							try {
+								JSONObject me = (JSONObject) new JSONTokener(
+										response).nextValue();
+								facebookId = me.getString("id");
+								facebookName = me.getString("name");
+								
+								List<NameValuePair> params = new ArrayList<NameValuePair>();
+								params.add(new BasicNameValuePair("uid", facebookId));
+								params.add(new BasicNameValuePair("name", facebookName));
+								postConnection("http://hillbomber.herokuapp.com/mobile_user.json", params);
+								
+								SharedPreferences.Editor editor = userPreferences.edit();
+								editor.putString("facebook_id", facebookId);
+								editor.putString("facebook_name", facebookName);
+								editor.commit();
+							} catch (JSONException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+					});
 				}
 
 				public void onFacebookError(FacebookError e) {
-//					Log.e("Facebook Error", e.toString());
+					// Log.e("Facebook Error", e.toString());
 				}
 
 				public void onError(DialogError e) {
-//					Log.e("Facebook Error", e.toString());
+					// Log.e("Facebook Error", e.toString());
 				}
 
 				public void onCancel() {
@@ -167,16 +237,16 @@ public class MainActivity extends MapActivity {
 		// Get the current location in start-up
 		centerLocation(new GeoPoint((int) (locationManager
 				.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-				.getLatitude() * 1000000), (int) (locationManager
+				.getLatitude() * 1E6), (int) (locationManager
 				.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-				.getLongitude() * 1000000)));
+				.getLongitude() * 1E6)));
 	}
-	
+
 	@Override
-	public void onResume() {    
-        super.onResume();
-        facebook.extendAccessTokenIfNeeded(this, null);
-    }
+	public void onResume() {
+		super.onResume();
+		facebook.extendAccessTokenIfNeeded(this, null);
+	}
 
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -190,13 +260,12 @@ public class MainActivity extends MapActivity {
 		// TODO Auto-generated method stub
 		return false;
 	}
-	
+
 	@Override
 	public void onBackPressed() {
-		if(routeView.getVisibility() == View.VISIBLE) {
+		if (routeView.getVisibility() == View.VISIBLE) {
 			routeView.setVisibility(View.GONE);
-		}
-		else {
+		} else {
 			super.onBackPressed();
 		}
 	}
@@ -206,32 +275,59 @@ public class MainActivity extends MapActivity {
 		titleText.setText("");
 		difficultyRadios.clearCheck();
 	}
-	
+
 	public void onStartRouteClicked(View v) {
 		if (!titleText.getText().toString().equals("")) {
 			currentRoute = titleText.getText().toString();
-			currentDifficulty = ((RadioButton)difficultyRadios.findViewById(difficultyRadios.getCheckedRadioButtonId())).getText().toString();
-			
+			currentDifficulty = ((RadioButton) difficultyRadios
+					.findViewById(difficultyRadios.getCheckedRadioButtonId()))
+					.getText().toString();
+			startLocation = locationManager
+					.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
 			refreshButton.setVisibility(View.GONE);
 			newButton.setVisibility(View.GONE);
 			endButton.setVisibility(View.VISIBLE);
 			routeView.setVisibility(View.GONE);
-		}
-		else {
-			Toast toast = Toast.makeText(getApplicationContext(), "Please title your new trail", Toast.LENGTH_SHORT);
+		} else {
+			Toast toast = Toast.makeText(getApplicationContext(),
+					"Please title your new trail", Toast.LENGTH_SHORT);
 			toast.show();
 		}
 	}
-	
+
 	public void onEndRouteClicked(View v) {
+		endLocation = locationManager
+				.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 		newButton.setVisibility(View.VISIBLE);
 		endButton.setVisibility(View.GONE);
 		refreshButton.setVisibility(View.VISIBLE);
+
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("uid", facebookId));
+		params.add(new BasicNameValuePair("name", facebookName));
+		params.add(new BasicNameValuePair("title", currentRoute));
+		params.add(new BasicNameValuePair("s_lat", Double
+				.toString(startLocation.getLatitude())));
+		params.add(new BasicNameValuePair("s_long", Double
+				.toString(startLocation.getLongitude())));
+		params.add(new BasicNameValuePair("e_lat", Double.toString(endLocation
+				.getLatitude())));
+		params.add(new BasicNameValuePair("e_long", Double.toString(endLocation
+				.getLongitude())));
+		params.add(new BasicNameValuePair("difficulty", Integer
+				.toString(difficultyToInteger(currentDifficulty))));
+
+		postConnection("http://hillbomber.herokuapp.com/trails.json", params);
+
+		Toast toast = Toast.makeText(getApplicationContext(), "Trail added!",
+				Toast.LENGTH_SHORT);
+		toast.show();
 	}
-	
+
 	public void onRefreshClicked(View v) {
 	}
-	
+
 	private InputStream getConnection(String url) {
 		InputStream is = null;
 		try {
@@ -245,13 +341,49 @@ public class MainActivity extends MapActivity {
 		return is;
 	}
 
+	public void postConnection(String url, List<NameValuePair> params) {
+		// Create a new HttpClient and Post Header
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpPost httppost = new HttpPost(url);
+
+		try {
+			// Add your data
+			httppost.setEntity(new UrlEncodedFormEntity(params));
+
+			// Execute HTTP Post Request
+			// HttpResponse response = httpclient.execute(httppost);
+			httpclient.execute(httppost);
+
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+		}
+	}
+
+	private int difficultyToInteger(String id) {
+		id = id.toLowerCase();
+		if (id.contains("green")) {
+			return 0;
+		} else if (id.contains("blue")) {
+			return 1;
+		} else if (id.contains("double")) {
+			return 2;
+		} else if (id.contains("black")) {
+			return 3;
+		} else {
+			return -1;
+		}
+
+	}
+
 	private class LongboardLocationListener implements LocationListener {
 
 		public void onLocationChanged(Location argLocation) {
 			// TODO Auto-generated method stub
 			GeoPoint geoPoint = new GeoPoint(
-					(int) (argLocation.getLatitude() * 1000000),
-					(int) (argLocation.getLongitude() * 1000000));
+					(int) (argLocation.getLatitude() * 1E6),
+					(int) (argLocation.getLongitude() * 1E6));
 
 			centerLocation(geoPoint);
 		}
